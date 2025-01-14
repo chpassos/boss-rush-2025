40980e83b25dccf534378f32b97ef3a0339141ec
chpassos <crlshnrqpassos@gmail.com>
2025-01-14 15:52:27 -0300

    Spawner implemented in rect (1920,1080)

--- scenes/spawner/spawner.gd
+++ scenes/spawner/spawner.gd
@@ -1,25 +1,16 @@
 extends Node2D
 
-
 @export var projectiles : PackedScene # Projectile scene to instantiate
 
 @onready var screen_size = get_viewport().get_visible_rect().size # get screen size
 
-
-
 # Spawn projectiles
 func spawn(pos: Vector2):
 	var projectile_instance = projectiles.instantiate()
-	
-	projectile_instance.position = pos
+	projectile_instance.global_position = pos
 	get_tree().current_scene.add_child(projectile_instance)
 
-# Based on our screen size, we get a random position from it
-func get_random_pos() -> Vector2:
+func _on_spawn_timer_timeout():
 	var random_x = randi_range(0, screen_size.x)
 	var random_y = randi_range(0, screen_size.y)
-	
-	return screen_size
-
-func _on_spawn_timer_timeout():
-	spawn(get_random_pos())
+	spawn(Vector2(random_x, random_y))
