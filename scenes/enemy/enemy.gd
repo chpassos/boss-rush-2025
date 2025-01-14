ff94edc78a5de79bdf226ef996d12fd8756fe749
Alfredo Reis <alf.reismarin@gmail.com>
2025-01-14 11:28:00 -0300

    Draw visual indication for the rotation detection's max distance

--- scenes/enemy/enemy.gd
+++ scenes/enemy/enemy.gd
@@ -1,9 +1,13 @@
 extends CharacterBody2D
 
 
-@onready var player_rotation_detection_component: PlayerRotationDetectionComponent = $PlayerRotationDetectionComponent
+@onready var rotation_detection: PlayerRotationDetectionComponent = $PlayerRotationDetectionComponent
 @onready var label: Label = $Label
 
 
+func _draw() -> void:
+	draw_circle(global_position, rotation_detection.max_distance, Color.BLACK, false)
+
+
 func _process(_delta: float) -> void:
-	label.text = str("%d" % rad_to_deg(player_rotation_detection_component.rotation_progress))
+	label.text = str("%d" % rad_to_deg(rotation_detection.rotation_progress))
