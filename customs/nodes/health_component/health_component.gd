5433e24dbfcb8092c0f3e66f547a2aa446628724
Alfredo Reis <alf.reismarin@gmail.com>
2025-01-16 16:08:08 -0300

    Add health to the Boss' scene

--- customs/nodes/health_component/health_component.gd
+++ customs/nodes/health_component/health_component.gd
@@ -1,47 +1,16 @@
-@icon("heart.svg")
 class_name HealthComponent
 extends Node
 
 
 signal health_reached_zero()
 
-@export_range(0, 10, 1, "or_greater") var max_health: int = 5:
-	set(mh):
-		set_max_health(mh, false)
+@export var max_health: int = 5
 
-@onready var health: int = max_health: set = _set_health
+@onready var current_health: int = max_health
 
 
-func _set_health(h: int) -> void:
-	health = clampi(h, 0, max_health)
+func take_damage(amount: int):
+	current_health = maxi(current_health - amount, 0)
 
-	if not health:
+	if not current_health:
 		health_reached_zero.emit()
-
-
-func set_max_health(mh: int, heal_diff: bool = true) -> void:
-	assert(mh > 0, "HealthComponent.max_health must be a positive value.")
-
-	if mh == max_health:
-		return
-
-	if mh > max_health:
-		var diff: int = mh - max_health
-		max_health = mh
-
-		if heal_diff:
-			health += diff
-
-	else:
-		max_health = mh
-		health = health
-
-
-func take_damage(value: int) -> void:
-	assert(value >= 0, "HealthComponent.take_damage expects a non-negative value.")
-	health -= value
-
-
-func heal(value: int, extra_to_shield: bool = false) -> void:
-	assert(value >= 0, "HealthComponent.take_damage expects a non-negative value.")
-	health += value
--- scenes/boss/boss.gd
+++ scenes/boss/boss.gd
@@ -1,3 +1,4 @@
+class_name Boss
 extends CharacterBody2D
 
 
@@ -5,8 +6,17 @@ extends CharacterBody2D
 @export_group("Nodes")
 @export var sprite: Sprite2D
 @export var light: PointLight2D
+@export var health_component: HealthComponent
 
 
 func _ready() -> void:
 	sprite.modulate = light_color
 	light.color = light_color
+
+
+func take_damage(value: int) -> void:
+	health_component.take_damage(value)
+
+
+func _on_health_component_health_reached_zero() -> void:
+	queue_free()
