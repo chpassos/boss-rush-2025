dc0f97f7c6e0bb9d80de67c779d24a5888aef79c
Alfredo Reis <alf.reismarin@gmail.com>
2025-01-29 11:48:18 -0300

    Add target attack to black hole boss

--- scenes/bosses/4_black_hole/black_hole.gd
+++ scenes/bosses/4_black_hole/black_hole.gd
@@ -4,11 +4,19 @@ extends Boss
 @export var gravity_field: Area2D
 @export var gravity_field_collision_shape: CollisionShape2D
 @export var circle_attack_spawner: Spawner
+@export var target_attack_spawner: Spawner
 
 
 func _ready() -> void:
 	super._ready()
-	take_damage(10, 0)
+
+	if not Globals.player:
+		SignalBus.player_ready.connect(
+			func():
+				target_attack_spawner.trackedNode = Globals.player
+		)
+	else:
+		target_attack_spawner.trackedNode = Globals.player
 
 
 func _on_event_horizon_body_entered(body: Node2D) -> void:
@@ -32,5 +40,12 @@ func _on_gravity_pull_state_exited() -> void:
 func _on_circle_attack_state_entered() -> void:
 	for _i in range(20):
 		circle_attack_spawner.set_manual_start(true)
-		#circle_attack_spawner.startRotation += 5
 		await get_tree().create_timer(0.25).timeout
+
+
+# TARGET ATTACK STATE
+
+func _on_target_attack_state_entered() -> void:
+	for _i in range(10):
+		target_attack_spawner.set_manual_start(true)
+		await get_tree().create_timer(0.15).timeout
