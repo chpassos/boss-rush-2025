7c8ac8114ab41d86fceb7fc6d83fd22cac3f540f
Alfredo Reis <alf.reismarin@gmail.com>
2025-01-31 15:58:29 -0300

    Implement black hole boss animations

--- scenes/bosses/4_black_hole/black_hole.gd
+++ scenes/bosses/4_black_hole/black_hole.gd
@@ -39,6 +39,7 @@ func _on_event_horizon_body_entered(body: Node2D) -> void:
 # GRAVITY PULL ATTACK STATE
 
 func _on_gravity_pull_state_entered() -> void:
+	anim_player.play(&"gravity_pull")
 	target_attack_count = 0
 	state_chart.set_expression_property(&"target_attack_count", target_attack_count)
 	homing_attack_count = 0
@@ -53,6 +54,7 @@ func _on_gravity_pull_state_exited() -> void:
 # CIRCLE ATTACK STATE
 
 func _on_circle_attack_state_entered() -> void:
+	anim_player.play(&"circle_attack")
 	for _i in range(20):
 		circle_attack_spawner.set_manual_start(true)
 		await get_tree().create_timer(0.25).timeout
@@ -62,6 +64,7 @@ func _on_circle_attack_state_entered() -> void:
 # TARGET ATTACK STATE
 
 func _on_target_attack_state_entered() -> void:
+	anim_player.play(&"target_attack")
 	target_attack_count += 1
 	state_chart.set_expression_property(&"target_attack_count", target_attack_count)
 	for _i in range(10):
@@ -73,9 +76,22 @@ func _on_target_attack_state_entered() -> void:
 # HOMING ATTACK STATE
 
 func _on_homing_attack_state_entered() -> void:
+	anim_player.play(&"homing_attack")
 	target_attack_count = 0
 	state_chart.set_expression_property(&"target_attack_count", target_attack_count)
 	homing_attack_count += 1
 	state_chart.set_expression_property(&"homing_attack_count", homing_attack_count)
 	homing_attack_spawner.set_manual_start(true)
 	state_chart.send_event(&"attack_finished")
+
+
+# IDLE STATE
+
+func _on_idle_state_entered() -> void:
+	if anim_player.is_playing():
+		if anim_player.current_animation == &"idle":
+			return
+		elif anim_player.current_animation == &"target_attack":
+			anim_player.play(&"idle")
+		else:
+			anim_player.queue(&"idle")
