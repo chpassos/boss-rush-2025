9d85dc4e66d43d0beb948327e93531f776610cb1
chpassos <crlshnrqpassos@gmail.com>
2025-02-02 08:33:27 -0300

    Added songs to SongManager

--- globals/song_manager.gd
+++ globals/song_manager.gd
@@ -1,12 +1,12 @@
 extends AudioStreamPlayer2D
 
 
-const FILLER_SONG: AudioStream = preload("res://assets/boss_rush_song5.wav")
-const MAIN_MENU_SONG: AudioStream = preload("res://assets/boss_rush_song5.wav")
-const RED_DWARF_BOSS_SONG: AudioStream = preload("res://assets/boss_rush_song5.wav")
-const BINARY_SYSTEM_BOSS_SONG: AudioStream = preload("res://assets/boss_rush_song5.wav")
-const BINARY_SYSTEM_FURY_BOSS_SONG: AudioStream = preload("res://assets/boss_rush_song5.wav")
-const BLACK_HOLE_BOSS_SONG: AudioStream = preload("res://assets/boss_rush_song5.wav")
+const FILLER_SONG: AudioStream = preload("res://assets/music/boss_rush_song5.wav")
+const MAIN_MENU_SONG: AudioStream = preload("res://assets/music/boss_rush_song5.wav")
+const RED_DWARF_BOSS_SONG: AudioStream = preload("res://assets/music/boss_rush_song3_v2.wav")
+const BINARY_SYSTEM_BOSS_SONG: AudioStream = preload("res://assets/music/boss_rush_song1.wav")
+const BINARY_SYSTEM_FURY_BOSS_SONG: AudioStream = preload("res://assets/music/boss_rush_phase2.wav")
+const BLACK_HOLE_BOSS_SONG: AudioStream = preload("res://assets/music/boss_rush_song5.wav")
 
 
 func _ready() -> void:
